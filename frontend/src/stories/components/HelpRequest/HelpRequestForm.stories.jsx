8e6f2fcc15984da5b2d908edba5adb4f59d72c9f
Terry Zhou <qinrui_zhou@ucsb.edu>
2025-11-04 09:40:10 -0800

    Form Component for HelpRequest plus tests

--- frontend/src/stories/components/HelpRequest/HelpRequestForm.stories.jsx
+++ frontend/src/stories/components/HelpRequest/HelpRequestForm.stories.jsx
@@ -1,36 +1,34 @@
 import React from "react";
 import HelpRequestForm from "main/components/HelpRequest/HelpRequestForm";
 import { helpRequestFixtures } from "fixtures/helpRequestFixtures";
+import { MemoryRouter } from "react-router";
 
 export default {
   title: "components/HelpRequest/HelpRequestForm",
   component: HelpRequestForm,
-  parameters: {
-    layout: "centered",
-  },
+  parameters: { layout: "centered" },
+  decorators: [(Story) => (<MemoryRouter><Story /></MemoryRouter>)],
 };
 
-const renderForm = (args) => {
-  return <HelpRequestForm {...args} />;
-};
+const Template = (args) => <HelpRequestForm {...args} />;
 
-export const Create = renderForm.bind({});
-
+export const Create = Template.bind({});
 Create.args = {
   buttonLabel: "Create",
   submitAction: (data) => {
-    console.log("Submit was clicked with data: ", data);
+    // eslint-disable-next-line no-console
+    console.log("Submit was clicked with data:", data);
     window.alert("Submit was clicked with data: " + JSON.stringify(data));
   },
 };
 
-export const EditExistingHelpRequest = renderForm.bind({});
-
-Update.args = {
+export const EditExistingHelpRequest = Template.bind({});
+EditExistingHelpRequest.args = {
   initialContents: helpRequestFixtures.oneHelpRequest,
   buttonLabel: "Update",
   submitAction: (data) => {
-    console.log("Submit was clicked with data: ", data);
+    // eslint-disable-next-line no-console
+    console.log("Submit was clicked with data:", data);
     window.alert("Submit was clicked with data: " + JSON.stringify(data));
   },
 };
