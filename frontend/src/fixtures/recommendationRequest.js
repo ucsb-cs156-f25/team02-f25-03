577a3a56cf3ce23359fa49ec39213d5fb9bd2bcb
Tarang Sharma <tarang@ucsb.edu>
2025-11-03 21:23:23 -0800

    Add fixture file for RecommendationRequest

---
+++ frontend/src/fixtures/recommendationRequest.js
@@ -0,0 +1,13 @@
+const recommendationRequest = {
+  onerequest: [
+    {
+      id: 0,
+      requesterEmail: "string",
+      professorEmail: "string",
+      explanation: "string",
+      dateRequested: "2025-11-04T04:58:52.791Z",
+      dateNeeded: "2025-11-04T04:58:52.791Z",
+      done: true
+    }
+  ]
+};
