31645b36589ac6bcbd803009f8a227b3b3eeabe7
JoshuaChu2004 <chujoshua.2004@gmail.com>
2025-11-06 13:07:19 -0800

    Update UCSBOrganizationCreatePage.test.jsx

--- frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationCreatePage.test.jsx
+++ frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationCreatePage.test.jsx
@@ -33,17 +33,6 @@ vi.mock("react-router", async (importOriginal) => {
 describe("UCSBOrganizationCreatePage tests", () => {
   const axiosMock = new AxiosMockAdapter(axios);
 
-  const setupUserOnly = () => {
-    axiosMock.reset();
-    axiosMock.resetHistory();
-    axiosMock
-      .onGet("/api/currentUser")
-      .reply(200, apiCurrentUserFixtures.userOnly);
-    axiosMock
-      .onGet("/api/systemInfo")
-      .reply(200, systemInfoFixtures.showingNeither);
-  };
-
   const queryClient = new QueryClient();
   test("renders without crashing", async () => {
     render(
