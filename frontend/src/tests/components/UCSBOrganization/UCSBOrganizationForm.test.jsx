b96e8117dd52ceb813598879733081e87038f666
JoshuaChu2004 <chujoshua.2004@gmail.com>
2025-11-06 13:06:42 -0800

    Update UCSBOrganizationForm.test.jsx

--- frontend/src/tests/components/UCSBOrganization/UCSBOrganizationForm.test.jsx
+++ frontend/src/tests/components/UCSBOrganization/UCSBOrganizationForm.test.jsx
@@ -45,9 +45,10 @@ describe("UCSBOrganization tests", () => {
     await screen.findByTestId("UCSBOrganizationForm-submit");
     const submitButton = screen.getByTestId("UCSBOrganizationForm-submit");
 
-    fireEvent.click(submitButton);
-
-    await screen.findByText(/Organization Translation Short is required./);
+    fireEvent.click(submitButton
+                    
+    await screen.findByText(/Organization Code is required./);
+    expect(screen.getByText(/Organization Translation Short is required./)).toBeInTheDocument();
     expect(screen.getByText(/Organization Translation is required./)).toBeInTheDocument();
   });
 
