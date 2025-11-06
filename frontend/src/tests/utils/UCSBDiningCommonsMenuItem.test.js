a0d8d286b3985c5a4e4cbf7d8d767aa496f4c7df
TOXICXUHUAN <58757783+TOXICXUHUAN@users.noreply.github.com>
2025-11-05 23:41:03 -0800

    Revert "Dylan ucsb dining commons menu item placeholder"

--- frontend/src/tests/utils/UCSBDiningCommonsMenuItem.test.js
+++ frontend/src/tests/utils/UCSBDiningCommonsMenuItem.test.js
@@ -41,7 +41,7 @@ describe("UCSBDiningCommonsMenuItemUtils", () => {
 
       // assert
       expect(result).toEqual({
-        url: "/api/diningcommonsmenuitem",
+        url: "/api/ucsbdiningcommonsmenuitem",
         method: "DELETE",
         params: { id: 17 },
       });
