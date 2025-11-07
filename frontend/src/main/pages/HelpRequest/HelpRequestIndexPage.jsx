c383ae44f75ef3e0d14ae338af26341f144417bf
Terry Zhou <qinrui_zhou@ucsb.edu>
2025-11-07 00:59:14 -0800

    all

--- frontend/src/main/pages/HelpRequest/HelpRequestIndexPage.jsx
+++ frontend/src/main/pages/HelpRequest/HelpRequestIndexPage.jsx
@@ -40,7 +40,10 @@ export default function HelpRequestIndexPage() {
       <div className="pt-2">
         {createButton()}
         <h1>helprequests</h1>
-        <HelpRequestTable helpRequests={helpRequests} currentUser={currentUser} />
+        <HelpRequestTable
+          helpRequests={helpRequests}
+          currentUser={currentUser}
+        />
       </div>
     </BasicLayout>
   );
