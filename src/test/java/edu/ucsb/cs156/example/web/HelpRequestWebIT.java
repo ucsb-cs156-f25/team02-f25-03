4615983d362dd8dde018d3fff991240fb79ccec0
terry <80630041+terryZhouqr@users.noreply.github.com>
2025-11-10 23:47:24 -0800

    Rename test method for clarity

---
+++ src/test/java/edu/ucsb/cs156/example/web/HelpRequestWebIT.java
@@ -0,0 +1,46 @@
+package edu.ucsb.cs156.example.web;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+import edu.ucsb.cs156.example.WebTestCase;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("integration")
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+public class HelpRequestWebIT extends WebTestCase {
+
+  @Test
+  public void admin_can_create_and_delete_helprequest() throws Exception {
+    setupUser(false);
+    page.getByText("Help Request").click();
+
+    page.getByText("Create HelpRequest").click();
+    assertThat(page.getByText("Create New HelpRequest")).isVisible();
+
+    page.getByTestId("HelpRequestForm-requesterEmail").fill("test@ucsb.edu");
+    page.getByTestId("HelpRequestForm-teamId").fill("7");
+    page.getByTestId("HelpRequestForm-tableOrBreakoutRoom").fill("14");
+    page.getByTestId("HelpRequestForm-requestTime").fill("2025-01-01T12:00");
+    page.getByTestId("HelpRequestForm-explanation").fill("Test explanation");
+    page.getByTestId("HelpRequestForm-solved").check();
+
+    page.getByTestId("HelpRequestForm-submit").click();
+
+    assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-requesterEmail"))
+        .hasText("test@ucsb.edu");
+
+    page.getByTestId("HelpRequestTable-cell-row-0-col-Delete-button").click();
+
+    assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-requesterEmail"))
+        .not()
+        .isVisible();
+  }
+}
